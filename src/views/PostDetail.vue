ac61c665615c8d9b8ec33685b3f5020af738bd1c
mysoyul <vega2k_@naver.com>
2021-09-09 16:42:43 +0900

    postDetail 추가

--- src/views/PostDetail.vue
+++ src/views/PostDetail.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
-    <h1>Post Detail page</h1>
+    <h2>View Post</h2>
+    <div v-if="loading">Loading...</div>
+    <div v-if="getPost">
+      <h3>[ID: {{ getPost.id }}]</h3>
+      <div>{{ getPost.text }}</div>
+    </div>
   </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  data() {
+    return {
+      loading: true,
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  watch: { $route: "fetchData" },
+  computed: {
+    ...mapGetters(['getPost'])
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+      this.$store.dispatch('loadPost', {id:this.$route.params.id} )
+      this.loading = false;
+    },
+  },
+};
+</script>
